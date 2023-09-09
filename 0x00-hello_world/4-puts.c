5f6499dbb42394c088ea78bc41ee764eaf89730e
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-09-09 16:44:30 -0700

    q4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdiio.h>
+
+/**
+main entry point
+Descrition: A C porgram that prints a word using Puts 
+return ALqayes 0
+*/
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+} 
