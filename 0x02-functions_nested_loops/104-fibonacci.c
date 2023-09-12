fbebd3dc6de77fe2e55da3a31fe4ae6e7e7ff319
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-09-12 14:33:15 -0700

    q16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,67 +1,44 @@
 #include <stdio.h>
 
-void print_number(int a, int b);
-
 /**
  * main - Prints first 98 Fibonacci numbers
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-    int a1 = 0, a2 = 1;
-    int b1 = 1, b2 = 2;
-    int next_a, next_b, carry = 0;
-    int i;
+	int carry = 0, i, j;
+	int a[100] = {0}, b[100] = {0}, next[100] = {0};
 
-    printf("%d, %d", a2, b2);
+	a[99] = 1;
+	b[99] = 2;
 
-    for (i = 2; i < 98; i++)
-    {
-        printf(", ");
-        next_a = a1 + b1 + carry;
-        next_b = a2 + b2;
+	printf("1, 2");
 
-        if (next_a > 999999999)
-        {
-            next_a %= 1000000000;
-            carry = 1;
-        }
-        else
-        {
-            carry = 0;
-        }
+	for (i = 0; i < 96; i++)
+	{
+		carry = 0;
+		printf(", ");
 
-        print_number(next_a, next_b);
+		for (j = 99; j >= 0; j--)
+		{
+			next[j] = a[j] + b[j] + carry;
+			carry = next[j] / 10;
+			next[j] %= 10;
+		}
 
-        a1 = b1;
-        a2 = b2;
-        b1 = next_a;
-        b2 = next_b;
-    }
+		for (j = 0; j < 100; j++)
+		{
+			a[j] = b[j];
+			b[j] = next[j];
+		}
 
-    printf("\n");
+		j = 0;
+		while (next[j] == 0)
+			j++;
 
-    return (0);
-}
-
-/**
- * print_number - Prints a large number
- * @a: First part
- * @b: Second part
- */
-void print_number(int a, int b)
-{
-    if (a != 0)
-    {
-        printf("%d", a);
-    }
-    if (a != 0 && b < 100000000)
-    {
-        printf("%09d", b);
-    }
-    else
-    {
-        printf("%d", b);
-    }
+		for (; j < 100; j++)
+			printf("%d", next[j]);
+	}
+	printf("\n");
+	return (0);
 }
