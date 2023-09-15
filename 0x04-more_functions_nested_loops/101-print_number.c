4318fc299b4f6c776a83f9ad070ded81071c7e38
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-09-15 12:40:11 -0700

    q12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,36 +1,22 @@
 #include "main.h"
 
 /**
- * print_recursive - Helper function to print each digit.
+ * print_number - Prints an integer.
  * @n: The integer.
- */
-void print_recursive(int n);
-
-/**
- * print_number - Prints an integer character by character.
- * @n: The integer to be printed.
  */
 void print_number(int n)
 {
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		if (n <= -10)
+			print_number(-n / 10);
+		_putchar(-n % 10 + '0');
 	}
-	if (n == 0)
-		_putchar('0');
 	else
-		print_recursive(n);
-}
-
-/**
- * print_recursive - Helper function to print each digit.
- * @n: The integer.
- */
-void print_recursive(int n)
-{
-	if (n == 0)
-		return;
-	print_recursive(n / 10);
-	_putchar((n % 10) + '0');
+	{
+		if (n >= 10)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
 }
