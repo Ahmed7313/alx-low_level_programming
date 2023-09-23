eb4d77ee71405ba999f88173f8113fac6842e382
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-09-23 16:12:26 -0700

    q11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,5 +6,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *reverse_string(char *str, int length)
 void _putchar(char c);
 #endif /* MAIN_H */
