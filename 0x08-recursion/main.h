7b70f50f6ea8115a1605c7e5599e68ec9b4bb8eb
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-09-30 14:14:53 -0700

    q0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-// Function prototypes and definitions go here
-
+/* Function prototypes and definitions go here */
 void _puts_recursion(char *s);
 void _putchar(char c);
 
