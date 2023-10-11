98b1384954082c6bfcabffd3b1b58d0af2555619
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-11 16:58:22 -0700

    q0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Describes the dog's basic information
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
