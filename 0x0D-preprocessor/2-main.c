c2e07171427daf6379d643f042962358904669b4
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-11 10:12:26 -0700

    q2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the file name of where the function is set
+ *
+ * Return: Alwayes 0
+ */
+
+int main(void)
+{
+	printf("%\n", __FILE__);
+	return (0);
+}
