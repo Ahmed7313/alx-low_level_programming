9e0df2a3adeed89586f32ea2883389ef93fbfec2
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-24 15:51:21 -0700

    q6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,16 @@
  */
 int pop_listint(listint_t **head)
 {
-    int n;
-    listint_t *temp;
+	int n;
+	listint_t *temp;
 
-    if (head == NULL || *head == NULL)
-        return (0);
+	if (head == NULL || *head == NULL)
+		return (0);
 
-    temp = *head;
-    *head = (*head)->next;
-    n = temp->n;
-    free(temp);
+	temp = *head;
+	*head = (*head)->next;
+	n = temp->n;
+	free(temp);
 
-    return (n);
+	return (n);
 }
