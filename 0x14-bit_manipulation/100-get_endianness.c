f2c18cc05b0e6749ed045c7f2c779a80370deed9
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-27 19:00:58 -0700

    q100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness of the system
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	union
+	{
+		unsigned int i;
+		char c[4];
+	} u;
+
+	u.i = 1;
+
+	return (u.c[0]);
+}
