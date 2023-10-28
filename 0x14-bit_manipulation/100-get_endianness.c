e3d136aa5161ea6e4c2adc65c5947954b9bddc8c
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-27 19:04:39 -0700

    q100

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * get_endianness - checks the endianness of the system
+ * get_endianness - Checks the endianness of the system.
  *
- * Return: 0 if big endian, 1 if little endian
+ * Return: 0 if big endian, 1 if little endian.
  */
 int get_endianness(void)
 {
-    /* Union to help in determining endianness */
-    union
-    {
-        unsigned int i;
-        char c[4];
-    } u;
+	/* Union to help in determining endianness */
+	union
+	{
+		unsigned int i;
+		char c[4];
+	} u;
 
-    u.i = 1;
+	u.i = 1;
 
-    return (u.c[0]);
+	return (u.c[0]);
 }
