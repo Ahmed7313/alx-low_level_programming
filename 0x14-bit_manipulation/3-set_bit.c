e0a6aa50ad40accda1f3ba5588f65e1c4a8146f6
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-10-27 18:53:18 -0700

    q3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,13 +8,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned long int mask;
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 
-    if (index >= sizeof(unsigned long int) * 8)
-        return (-1);
+	unsigned long int mask = 1UL << index;
+	*n |= mask;
 
-    mask = 1UL << index;
-    *n |= mask;
-
-    return (1);
+	return (1);
 }
