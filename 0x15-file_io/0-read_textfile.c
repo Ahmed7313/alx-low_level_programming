bb1e0a2bca135de955468be88d5b174baf11dcee
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-11-03 14:32:08 -0700

    q0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,14 @@
 #include "main.h"
-#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-/**
- * open_file - Opens a file
- * @filename: The name of the file to open
- * Return: File descriptor on success, -1 on failure
- */
-static int open_file(const char *filename)
-{
-	return (open(filename, O_RDONLY));
-}
-
-/**
- * read_file - Reads from a file
- * @fd: The file descriptor to read from
- * @buffer: The buffer to read the data into
- * @letters: The number of letters to read and print
- * Return: Number of letters it could read and print
- */
-static ssize_t read_file(int fd, char *buffer, size_t letters)
-{
-	return (read(fd, buffer, letters));
-}
-
-/**
- * write_to_stdout - Writes to standard output
- * @buffer: The buffer containing the data to write
- * @num: The number of bytes to write
- * Return: Number of bytes it could write
- */
-static ssize_t write_to_stdout(char *buffer, ssize_t num)
-{
-	return (write(STDOUT_FILENO, buffer, num));
-}
+#include <fcntl.h>
 
 /**
  * read_textfile - Reads a text file and prints it to the POSIX standard output
- * @filename: The name of the file to read
+ * @filename: The name of the file to be read
  * @letters: The number of letters it should read and print
- * Return: Actual number of letters it could read and print
+ *
+ * Return: Actual number of letters it could read and print, or 0 if it fails
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -51,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open_file(filename);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
@@ -62,15 +30,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	nread = read_file(fd, buffer, letters);
-	if (nread > 0)
-		nwritten = write_to_stdout(buffer, nread);
-
-	free(buffer);
+	nread = read(fd, buffer, letters);
 	close(fd);
+	if (nread == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-	if (nread > 0 && nwritten == nread)
-		return (nwritten);
+	nwritten = write(STDOUT_FILENO, buffer, nread);
+	free(buffer);
+	if (nwritten == -1 || nwritten != nread)
+		return (0);
 
-	return (0);
+	return (nwritten);
 }
